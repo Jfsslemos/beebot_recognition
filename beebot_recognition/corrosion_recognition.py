77d90ffbb5187f8a6f256678a8a2490e4d12ad6d
jfsslemos <jfsslemos@gmail.com>
2023-10-19 14:34:20 -0300

    Working version of the corrosion recognition node

--- beebot_recognition/corrosion_recognition.py
+++ beebot_recognition/corrosion_recognition.py
@@ -1,6 +1,11 @@
 import rclpy
 from rclpy.node import Node
-
+from sensor_msgs.msg import Image
+from ultralytics import YOLO
+import numpy as np
+from cv_bridge import CvBridge
+from PIL import Image as IMG
+import numpy
 class TestYAMLParams(Node):
 
     def __init__(self):
@@ -19,6 +24,21 @@ class TestYAMLParams(Node):
                 ('bytes_array', None),
                 ('nested_param.another_int', None)
             ])
+        self.publisher_ = self.create_publisher(Image, 'debug_img', 1)
+        self.subscription = self.create_subscription(Image, '/image_raw', self.callback, qos_profile=1)
+        self.subscription
+        self.model = YOLO('/home/joao/Downloads/best.pt')
+        self.cv_bridge = CvBridge()
+
+    def callback(self, msg):
+        img = self.cv_bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
+        results = self.model(img)
+        for r in results:
+            im_array = r.plot()  # plot a BGR numpy array of predictions
+            im = IMG.fromarray(im_array[..., ::-1])  # RGB PIL image
+        img_writen = self.cv_bridge.cv2_to_imgmsg(numpy.array(im), encoding='rgb8')
+        #self.get_logger().info(f"Received: {results}")
+        self.publisher_.publish(img_writen)
 
 def main(args=None):
     rclpy.init(args=args)
