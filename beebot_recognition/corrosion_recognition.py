820acff56d3635233b1bb5e9b3f8cd7a53b6b1b6
jfsslemos <jfsslemos@gmail.com>
2023-10-20 22:18:12 -0300

    making corrosion node changeble by parameters

--- beebot_recognition/corrosion_recognition.py
+++ beebot_recognition/corrosion_recognition.py
@@ -6,43 +6,45 @@ import numpy as np
 from cv_bridge import CvBridge
 from PIL import Image as IMG
 import numpy
-class TestYAMLParams(Node):
+class BeebotRecognition(Node):
 
     def __init__(self):
-        super().__init__('your_amazing_node')
+        super().__init__('corrosion_recogniton_node')
         self.declare_parameters(
             namespace='',
             parameters=[
-                ('bool_value', None),
-                ('int_number', None),
-                ('float_number', None),
-                ('str_text', None),
-                ('bool_array', None),
-                ('int_array', None),
-                ('float_array', None),
-                ('str_array', None),
-                ('bytes_array', None),
-                ('nested_param.another_int', None)
+                ('subscribers.image_rgb', rclpy.Parameter.Type.STRING),
+                ('publishers.debug_image', rclpy.Parameter.Type.STRING),
+                ('publishers.recognition_topic', rclpy.Parameter.Type.STRING)
             ])
-        self.publisher_ = self.create_publisher(Image, 'debug_img', 1)
-        self.subscription = self.create_subscription(Image, '/image_raw', self.callback, qos_profile=1)
+        self.readParams()
+        self.get_logger().info(f"Received: {self.debug_image_topic}")
+        self.get_logger().info(f"Received: {self.image_rgb}")
+        self.get_logger().info(f"Received: {self.recognition_topic}")
+        self.publisher_ = self.create_publisher(Image, self.debug_image_topic, 1)
+        self.subscription = self.create_subscription(Image, self.image_rgb, self.callback, qos_profile=1)
         self.subscription
-        self.model = YOLO('/home/joao/Downloads/best.pt')
+        self.model = YOLO('yolov8n.pt')
         self.cv_bridge = CvBridge()
-
+    
     def callback(self, msg):
         img = self.cv_bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
         results = self.model(img)
         for r in results:
-            im_array = r.plot()  # plot a BGR numpy array of predictions
-            im = IMG.fromarray(im_array[..., ::-1])  # RGB PIL image
+            im_array = r.plot()
+            im = IMG.fromarray(im_array[..., ::-1])
         img_writen = self.cv_bridge.cv2_to_imgmsg(numpy.array(im), encoding='rgb8')
         #self.get_logger().info(f"Received: {results}")
         self.publisher_.publish(img_writen)
+    
+    def readParams(self):
+        self.debug_image_topic = self.get_parameter('publishers.debug_image').value
+        self.image_rgb = self.get_parameter('subscribers.image_rgb').value
+        self.recognition_topic = self.get_parameter('publishers.recognition_topic').value
 
 def main(args=None):
     rclpy.init(args=args)
-    node = TestYAMLParams()
+    node = BeebotRecognition()
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
